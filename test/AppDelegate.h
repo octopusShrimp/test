059eec49ed461f452a6b71dc9500edd3898d0de7
longlong <longlong@longlongdemacbook-air.local>
2016-04-10 17:50:09 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by longlong on 16/4/10.
+//  Copyright © 2016年 longlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
